1a69ad3140e65685e440878275e9649ea820461f
CTO at UaStar.com <CTO@UaStar.dev>
2022-11-11 21:55:27 +0200

    Added balances to CashFlowStatement to make it work.

--- src/Reports/CashFlowStatement.php
+++ src/Reports/CashFlowStatement.php
@@ -99,6 +99,9 @@ class CashFlowStatement extends FinancialStatement
         $this->balances[self::NON_CURRENT_ASSETS] = 0;
         $this->balances[self::NON_CURRENT_LIABILITIES] = 0;
         $this->balances[self::EQUITY] = 0;
+        $this->balances[self::PROFIT] = 0;
+        $this->balances[self::START_CASH_BALANCE] = 0;
+        $this->balances[self::NET_CASH_FLOW] = 0;
 
         // Statement Results
         $this->results[self::OPERATIONS_CASH_FLOW] = 0;
